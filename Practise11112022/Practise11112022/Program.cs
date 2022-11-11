9eba691fe228baa3da777d697dbe71074bb566eb
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-11 08:17:04 +0530

    [Afzal] added: Folder Structure

---
+++ Practise11112022/Practise11112022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practise11112022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
