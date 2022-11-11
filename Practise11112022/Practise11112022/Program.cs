6d2caa250c37f6c1d81a73dbe812c5c3216dc967
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-11 08:56:49 +0530

    [Afzal] Added: Multiplication Table

--- Practise11112022/Practise11112022/Program.cs
+++ Practise11112022/Practise11112022/Program.cs
@@ -9,7 +9,8 @@ namespace Practise11112022
             Console.WriteLine("Practise Set 2");
             Console.WriteLine("Enter the option");
             Console.WriteLine("1. Print Alphabets A to Z"+"\n"+
-                              "2. ASCII values of all characters"+"\n");
+                              "2. ASCII values of all characters"+"\n"+
+                              "3. Multiplication Table"+"\n");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -20,6 +21,10 @@ namespace Practise11112022
                 case 2:
                     ASCIICharacters printASCIICharacters = new ASCIICharacters();
                     printASCIICharacters.ASCII();
+                    break;
+                case 3:
+                    MultiplicationTable multiplicationTable = new MultiplicationTable();
+                    multiplicationTable.table();
                     break;
             }
         }
