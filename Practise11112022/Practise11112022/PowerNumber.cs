edfb8187fe40a05fb15178bfe2db55ffcb1adba4
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-11 09:13:39 +0530

    [Afzal] added : Number Power using two methods

---
+++ Practise11112022/Practise11112022/PowerNumber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practise11112022
+{
+    public class PowerNumber
+    {
+        public void PowerUsingWhile()
+        {
+            int basenumber, exponent;
+
+            Console.Write("Enter basenumber number: ");
+            basenumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter exponent number: ");
+            exponent = Convert.ToInt32(Console.ReadLine());
+
+            int power = 1;
+            int i = 1;
+            
+            while (i <= exponent)
+            {
+                power = power * basenumber;
+                i++;
+            }
+            Console.Write("Power : " + power);
+
+            Console.ReadLine();
+
+        }
+
+        public void PowerUsingFor()
+        {
+            int basenumber, exponent;
+
+            Console.Write("Enter basenumber number: ");
+            basenumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter exponent number: ");
+            exponent = Convert.ToInt32(Console.ReadLine());
+
+            int power = 1;
+            for (int i = 1; i <= exponent; i++)
+            {
+                power = power * basenumber;
+            }                
+            Console.Write("Power : " + power);
+            Console.ReadLine();
+        }
+    }
+}
--- Practise11112022/Practise11112022/Program.cs
+++ Practise11112022/Practise11112022/Program.cs
@@ -10,7 +10,8 @@ namespace Practise11112022
             Console.WriteLine("Enter the option");
             Console.WriteLine("1. Print Alphabets A to Z"+"\n"+
                               "2. ASCII values of all characters"+"\n"+
-                              "3. Multiplication Table"+"\n");
+                              "3. Multiplication Table"+"\n"+
+                              "4. Power Number");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -26,6 +27,20 @@ namespace Practise11112022
                     MultiplicationTable multiplicationTable = new MultiplicationTable();
                     multiplicationTable.table();
                     break;
+                case 4:
+                    PowerNumber powerNumber = new PowerNumber();
+                    Console.WriteLine("Enter the method number ");
+                    Console.WriteLine("1.using While loop"+"\n"+
+                                        "2.using For loop");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    if (num == 1)
+                    {
+                        powerNumber.PowerUsingWhile();
+                    }else
+                        powerNumber.PowerUsingFor();
+                    break;
+                        
+
             }
         }
     }
