85ade0cec88d96971586773ec7d8027591204f75
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-11 08:45:29 +0530

    [Afzal] added: ASCII for all characters

---
+++ Practise11112022/Practise11112022/ASCIICharacters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practise11112022
+{
+    public class ASCIICharacters
+    {
+        public void ASCII()
+        {
+            char c;
+            for (c = 'A'; c <= 'Z'; c++)
+            {
+                int num = Convert.ToInt32(c);
+                Console.WriteLine("The ASCII value {0} is {1} ",c,num );
+            }
+        }
+    }
+}
--- Practise11112022/Practise11112022/Program.cs
+++ Practise11112022/Practise11112022/Program.cs
@@ -8,13 +8,18 @@ namespace Practise11112022
         {
             Console.WriteLine("Practise Set 2");
             Console.WriteLine("Enter the option");
-            Console.WriteLine("1. Print Alphabets A to Z"+"\n");
+            Console.WriteLine("1. Print Alphabets A to Z"+"\n"+
+                              "2. ASCII values of all characters"+"\n");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
                 case 1:
                     PrintAlphabets printAlphabets = new PrintAlphabets();
                     printAlphabets.Aplhabets();
+                    break;
+                case 2:
+                    ASCIICharacters printASCIICharacters = new ASCIICharacters();
+                    printASCIICharacters.ASCII();
                     break;
             }
         }
