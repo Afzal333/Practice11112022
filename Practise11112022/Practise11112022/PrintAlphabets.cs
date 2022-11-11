ac91c414e5ec463af1abef1d3a42e3226d7803d4
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-11 08:28:55 +0530

    [Afzal] Added: Alphabets A to Z

---
+++ Practise11112022/Practise11112022/PrintAlphabets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practise11112022
+{
+    public class PrintAlphabets
+    {
+        public void Aplhabets()
+        {
+            char c;
+            Console.WriteLine( "\n");
+            for (c = 'A'; c <= 'Z'; c++)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
--- Practise11112022/Practise11112022/Program.cs
+++ Practise11112022/Practise11112022/Program.cs
@@ -6,7 +6,17 @@ namespace Practise11112022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Practise Set 2");
+            Console.WriteLine("Enter the option");
+            Console.WriteLine("1. Print Alphabets A to Z"+"\n");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    PrintAlphabets printAlphabets = new PrintAlphabets();
+                    printAlphabets.Aplhabets();
+                    break;
+            }
         }
     }
 }
